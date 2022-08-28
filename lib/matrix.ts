6547c1355ec3548a9fa57c3b6456215ba4167ca1
Taro Matsuzawa <btm@tech.email.ne.jp>
2022-08-28 15:45:59 +0200

    bugfix: enable to diffrent size images

--- lib/matrix.ts
+++ lib/matrix.ts
@@ -22,18 +22,25 @@ export class Matrix {
     if (this.position_y < this.max_y || this.position_y === 0) {
       this.position_y += image.real_height()
       if (this.max_x < this.position_x + image.real_width()) {
-        this.max_x = this.position_x + image.real_width()
+        this.max_x += image.real_width()
       }
-      if (this.position_y === this.max_y) {
+      if (this.position_y >= this.max_y) {
         this.position_x = 0
+        this.max_y = this.position_y
       }
     } else if (this.position_x < this.max_x) {
       this.position_x += image.real_width()
+      if (this.position_x > this.max_x) {
+        this.max_x = this.position_x
+      }
     } else {
       if (this.max_y <= this.position_y + image.real_height()) {
         this.max_y += image.real_height()
       }
       this.position_y = 0
+      if (this.max_x <= this.position_x + image.real_width()) {
+        this.max_x += image.real_width()
+      }
       this.position_x = this.max_x
     }
   }
