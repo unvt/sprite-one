ad3bb5ebfbd896e38d9636ec896a1f78aa9a1dd1
Keitaroh Kobayashi <keita@kbys.me>
2022-11-28 14:21:59 +0900

    add failing test for 2 images

---
+++ tests/lib/matrix.test.ts
@@ -0,0 +1,48 @@
+import path from 'node:path'
+
+import { Image } from '../../src/lib/image'
+import { Matrix } from '../../src/lib/matrix'
+
+const iconsDir = path.join(__dirname, '../icons')
+const icons2Dir = path.join(__dirname, '../icons2')
+// const images2 = [
+//   new Image(path.join(icons2Dir, 'arrow.svg'), 2),
+//   new Image(path.join(icons2Dir, 'circle.svg'), 2),
+// ]
+
+describe('test lib/matrix.ts', (): void => {
+  test('matrix calculation works with 1 image', async () => {
+    const images1 = [
+      await new Image(path.join(iconsDir, 'airport.svg'), 1).parse(),
+      // await new Image(path.join(iconsDir, 'airport2.svg'), 1).parse(),
+    ]
+
+    expect(images1[0].height).toStrictEqual(15);
+    expect(images1[0].width).toStrictEqual(15);
+
+    const matrix = new Matrix(images1)
+    matrix.calc()
+    
+    expect(matrix.max_x).toStrictEqual(15);
+    expect(matrix.max_y).toStrictEqual(15);
+  })
+
+  test('matrix calculation works with 2 images', async () => {
+    const images1 = [
+      await new Image(path.join(iconsDir, 'airport.svg'), 1).parse(),
+      await new Image(path.join(iconsDir, 'airport2.svg'), 1).parse(),
+    ]
+
+    expect(images1[0].height).toStrictEqual(15);
+    expect(images1[0].width).toStrictEqual(15);
+    expect(images1[1].height).toStrictEqual(15);
+    expect(images1[1].width).toStrictEqual(15);
+
+    const matrix = new Matrix(images1)
+    matrix.calc()
+    
+    expect(matrix.max_x).toStrictEqual(15);
+    expect(matrix.max_y).toStrictEqual(30);
+  })
+
+})
